add90267b80128d4309d60af92d13e3ca16a6f7f
lambert <lambert.rocher@jellysmack.com>
2023-01-09 23:10:09 +0100

    replace eventListener by anchor

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import logo from "./logo.png";
 import "./App.css";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const button = document.querySelector('.button');
-
-    button.addEventListener('click', () => {
-      const redirectUrl = 'https://discord.com/api/oauth2/authorize?client_id=1041459238318379068&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fblueprints%2Flogin&response_type=code&scope=identify%20email';
-      window.location.href = redirectUrl;
-    });
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Cosmoteer Ships
-          <br/>
+          <br />
           Coming soon !
         </p>
       </header>
       <div className="Body">
-        <button className="button" type="button">
-          Login with discord
-        </button>
+        <a href="https://discord.com/api/oauth2/authorize?client_id=1041459238318379068&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fblueprints%2Flogin&response_type=code&scope=identify%20email">
+          <button className="button" type="button">
+            Login with discord
+          </button>
+        </a>
       </div>
     </div>
   );
