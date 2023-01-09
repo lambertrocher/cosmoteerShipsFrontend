626bee397f17b7a8798e53c3f714c36a2a5e6edb
lambert <lambert.rocher@jellysmack.com>
2023-01-09 22:34:49 +0100

    login with discord button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const button = document.querySelector('.button');
+
+    button.addEventListener('click', () => {
+      const redirectUrl = 'https://discord.com/api/oauth2/authorize?client_id=1041459238318379068&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fblueprints%2Flogin&response_type=code&scope=identify%20email';
+      window.location.href = redirectUrl;
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cosmoteer Ships
+          <br/>
+          Coming soon !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="Body">
+        <button className="button" type="button">
+          Login with discord
+        </button>
+      </div>
     </div>
   );
 }
